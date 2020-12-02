cb2ff6bf3bb42366bc3a5bc871fa156946df3a38
JanEveraertEHB <72449780+JanEveraertEHB@users.noreply.github.com>
2020-12-02 11:52:45 +0100

    Test: add end-to-end test
    adding staging changes
    
    adding prebuild
    
    add typescreipt to build
    
    test circleCI
    
    update and remove
    
    checkup CCI
    
    remove certain tests
    
    update docker

--- api/src/__tests__/unit.js
+++ api/src/__tests__/unit.js
@@ -1,18 +1,24 @@
 
-const Conversationhelpers = require('../helper/ConversationHelpers.js')
+const ConversationhelpersImport = require('../helper/ConversationHelpers.js')
 
 describe('check if helper senses emotion', () => {
   test('check on bad input', () => {
-    expect(Conversationhelpers.senseEmotionHelper(null)).toBe(null);
-    expect(Conversationhelpers.senseEmotionHelper(123)).toBe(null);
-    expect(Conversationhelpers.senseEmotionHelper("")).toBe(null);
-    expect(Conversationhelpers.senseEmotionHelper("a")).toBe(null);
+    expect(ConversationhelpersImport.senseEmotionHelper(null)).toBe(null);
+    expect(ConversationhelpersImport.senseEmotionHelper(123)).toBe(null);
+    expect(ConversationhelpersImport.senseEmotionHelper("")).toBe(null);
+    expect(ConversationhelpersImport.senseEmotionHelper("a")).toBe(null);
   })
   test('check on no question', () => {
-    expect(Conversationhelpers.senseEmotionHelper('hi there.')).toBe(1);
+    expect(ConversationhelpersImport.senseEmotionHelper('hi there.')).toBe(1);
   })
   test('check if good comes through', () => {
-    expect(Conversationhelpers.senseEmotionHelper("Don't you like this?")).toBe(2);
-    expect(Conversationhelpers.senseEmotionHelper("Do you like this?")).toBe(2);
+    expect(ConversationhelpersImport.senseEmotionHelper("Don't you like this?")).toBe(2);
+    expect(ConversationhelpersImport.senseEmotionHelper("Do you like this?")).toBe(2);
+  })
+
+  test('interpret emotion value', () => {
+    expect(ConversationhelpersImport.convertEmotionValue(0)).toHaveProperty("emoji", ":|")
+    expect(ConversationhelpersImport.convertEmotionValue(-1)).toHaveProperty("emoji", ":(")
+    expect(ConversationhelpersImport.convertEmotionValue(1)).toHaveProperty("emoji", ":)")
   })
 })
--- api/src/index.js
+++ api/src/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
-const ConversationHelpers = require('./helper/ConversationHelpers')
-const port = process.env.PORT
+const ConversationHelpers = require('./helper/ConversationHelpers');
+const DatabaseHelper = require('./helper/DatabaseHelper');
+const UUIDHelper = require('./helper/UuidHelpers');
 
 app.use(bodyParser.json());
 app.use(
@@ -10,34 +11,110 @@ app.use(
     // to support URL-encoded bodies
     extended: true
   })
-);  
+);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-
 })
 
 /**
 * 
 */
-app.post('/question', (req, res) => {
+app.post('/question', async (req, res) => {
   const question = req.body.question;
-  const response  = ConversationHelpers.senseEmotionHelper(question);
-  if(response == null) {
-    res.sendStatus(400)
+  const response = await ConversationHelpers.senseEmotionHelper(question)
+  const uuid = UUIDHelper.generateUUID();
+  if (response) {
+    const toInsertQuestion = {
+      uuid: uuid,
+      question: question,
+      answer: response.toString()
+    }
+    await DatabaseHelper.insert(toInsertQuestion).table('records').returning('*').then(async (data) => {
+      if (response == null) {
+        res.sendStatus(400)
+      }
+      else {
+        const answer = { ...ConversationHelpers.convertEmotionValue(response), uuid: uuid };
+        res.send(answer);
+      }
+    }).catch((e) => {
+      res.status(400).send(e)
+    })
   }
   else {
-    if(response <= 0) {
-      res.send({answer: ":(" })
-    } else {
-      res.send({answer: ":)" })
-    }
+    res.status(400).send()
   }
-  
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.get('/questions', async (req, res) => {
+  await DatabaseHelper.table('records').select('*').then((data) => {
+    res.send(data);
+  }).catch((error) => {
+    res.send(error).status(400)
+  })
 })
+
+app.get('/question/:uuid', async (req, res) => {
+  if (req.params.uuid) {
+    await DatabaseHelper.table('records').select('*').where({ uuid: req.params.uuid }).then((data) => {
+      if (data.length > 0) {
+        res.send(data[0]);
+      }
+      else {
+        // could not find
+        res.sendStatus(404)
+      }
+    }).catch((error) => {
+      res.send(error).status(400)
+    })
+  }
+  else {
+    res.send(400)
+  }
+})
+
+app.patch('/question/:uuid', async (req, res) => {
+  if (req.params.uuid && req.body) {
+    const toAlter = {};
+    if (req.body.question) {
+      toAlter["question"] = req.body.question;
+    }
+    await DatabaseHelper.table('records').update(toAlter).where({ uuid: req.params.uuid }).returning('*').then((data) => {
+      if (data.length > 0) {
+        res.status(200).send(data[0]);
+      }
+      else {
+        res.status(404).send();
+      }
+    }).catch((error) => {
+      res.status(403).send(error)
+    })
+  }
+  else {
+    res.sendStatus(400)
+  }
+})
+app.delete('/question/:uuid', async (req, res) => {
+  if (req.params.uuid) {
+    await DatabaseHelper.table('records').delete().where({ uuid: req.params.uuid }).returning('*').then((data) => {
+      if (data.length > 0) {
+        res.sendStatus(200);
+      }
+      else {
+        res.sendStatus(404);
+      }
+    }).catch((error) => {
+      res.send(error).status(400)
+    })
+  }
+  else {
+    res.send(400)
+  }
+})
+
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`));
+}
 
 module.exports = app
